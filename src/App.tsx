fcce57fb3f199a4176e54c0c1b50ea742051a7f6
何昌举 <hechangju@mat.ai>
2020-11-27 13:45:20 +0800

    feat: context style

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, createContext } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BatteryContext = createContext(60);
+const OnlineContext = createContext(false);
+
+class Middel extends React.Component {
+  render() {
+    return (
+      <Leaf />
+    )
+  }
 }
+
+class Leaf extends React.Component {
+  static contextType = BatteryContext;
+  render() {
+    const battery = this.context;
+    console.log(battery);
+    return (
+      <h1>battery: {battery}</h1>
+    )
+  }
+}
+
+
+interface AppState {
+  battery: number,
+}
+
+class App extends React.Component {
+
+  state: AppState = {
+    battery: 60,
+  }
+
+  render() {
+    const { battery } = this.state;
+    return (
+      <div className="App">
+        <BatteryContext.Provider value={battery}>
+          <button type="button" onClick={() => this.setState({ battery: battery - 1 })}>clie me</button>
+          <Middel />
+        </BatteryContext.Provider>
+      </div>
+    )
+  }
+
+}
+
 
 export default App;
