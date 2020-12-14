771b8673927bcd0d00b74eb0bd10cef8600edbf6
何昌举 <hechangju@mat.ai>
2020-12-14 11:17:18 +0800

    feat: hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useCallback, memo, useMemo, useRef } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 
-function App() {
+
+function App () {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <nav>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/other">other</Link>
+          </li>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Other />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
+  )
 }
+
+function About () {
+  return (
+    <div>about</div>
+  )
+}
+
+function Home () {
+  return (
+    <div>home</div>
+  )
+}
+function Other () {
+  return (
+    <div>other</div>
+  )
+}
+
 
 export default App;
